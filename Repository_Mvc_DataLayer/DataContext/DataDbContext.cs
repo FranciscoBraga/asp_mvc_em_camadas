6f7dab06c210b7f914f24b86e2b5e488b1b2d8bd
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-14 11:02:45 -0200

    Instalado EntityFrameWork e criado o a classe de Contexto

---
+++ Repository_Mvc_DataLayer/DataContext/DataDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Repository_Mvc_DataLayer.DataContext
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext():base("Name=BDRepository")
+        {
+
+        }
+    }
+}
