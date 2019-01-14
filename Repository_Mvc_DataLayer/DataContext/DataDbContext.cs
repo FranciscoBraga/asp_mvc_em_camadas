e9bb59a9a535c68f241c742ec5d989fa4546c2e7
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-14 11:27:42 -0200

    Criada Primeira Entidade e Colocada a  connectionString no arquivo App.config

--- Repository_Mvc_DataLayer/DataContext/DataDbContext.cs
+++ Repository_Mvc_DataLayer/DataContext/DataDbContext.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
+using Repository_Mvc_DataLayer.DataEntity;
 
 namespace Repository_Mvc_DataLayer.DataContext
 {
     public class DataDbContext : DbContext
     {
-        public DataDbContext():base("Name=BDRepository")
+        public DataDbContext():base("BDRepository")
         {
+           
 
         }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ Repository_Mvc_DataLayer/DataEntity/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository_Mvc_DataLayer.DataEntity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+    }
+}
