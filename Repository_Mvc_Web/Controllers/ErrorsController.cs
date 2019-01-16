04dacb4605c8dc1915ff68926020fe2aaec648db
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-16 11:37:08 -0200

    Página de erro 404 e 500

---
+++ Repository_Mvc_Web/Controllers/ErrorsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Repository_Mvc_Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult General()
+        {
+            return View();
+        }
+
+        public ActionResult Http404()
+        {
+            return View();
+        }
+
+        public ActionResult Http500()
+        {
+            return View();
+        }
+    }
+}
--- Repository_Mvc_Web/Global.asax.cs
+++ Repository_Mvc_Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository_Mvc_Web.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,38 @@ namespace Repository_Mvc_Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var app = (MvcApplication)sender;
+            var context = app.Context;
+            var ex = app.Server.GetLastError();
+            context.Response.Clear();
+            context.ClearError();
+
+            var httpException = ex as HttpException;
+
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "errors";
+            routeData.Values["exception"] = ex;
+            routeData.Values["action"] = "http500";
+
+            if (httpException != null)
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        routeData.Values["action"] = "Http404";
+                        break;
+                    case 500:
+                        routeData.Values["action"] = "Http500";
+                        break;
+                }
+            }
+
+            IController controller = new ErrorsController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(context), routeData));
         }
     }
 }
